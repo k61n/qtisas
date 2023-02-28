aba41a5fe00e3ef4657dd2e8b2fe0c8b0f5dfa50
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2023-02-28 12:33:49 +0100

    libtamuanova build script

---
+++ scripts/build-tamuanova.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo Building tamu-anova library
+
+file="../lib/$1-$2/libtamuanova.a"
+
+if [ -f "$file" ]; then
+	echo "Tamu-anova library is already built: $file"
+  exit 1
+fi
+
+cd $4
+mkdir build
+rsync -av --exclude=build . build/ > /dev/null 2>&1
+cd build/
+
+./configure -q --disable-shared CFLAGS=-I"../../gsl/include/"
+
+if [[ $1 == "Darwin" ]]; then
+  gmake -s -j $3 > gmake.log 2>&1
+else
+  make -s -j $3 > make.log 2>&1
+fi
+
+if [ $? -ne 0 ]; then
+  echo Error while compiling
+  exit 1
+fi
+
+cp libtamuanova.a ../../../lib/$1-$2
+
+cd ..
+rm -rf build
+
+exit 0
