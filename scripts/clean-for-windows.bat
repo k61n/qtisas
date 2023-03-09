6310f6405a669027916ddb7fcc66b4b6fdd4f361
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2023-03-09 17:39:42 +0100

    some scripts not needed

--- scripts/clean-for-windows.bat
+++
@@ -1,8 +0,0 @@
-echo ... removing makefiles ...
-@ECHO OFF
-del 3rdparty\qwt\src\Makefil*
-del 3rdparty\qwt\Makefil*
-del 3rdparty\qwtplot3d\Makefil*
-del 3rdparty\qtexengine\src\Makefil*
-del 3rdparty\qtexengine\Makefil*
-del pro\Makefil*
