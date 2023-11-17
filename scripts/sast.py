c9ce782ae129a206c0039ee326b9f0161768f5e3
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2023-11-17 18:29:39 +0100

    sast.py: handle when clang-tidy exits with status 1
    This happens when clang-tidy finds compiler errors also in non-related
    files.

--- scripts/sast.py
+++ scripts/sast.py
@@ -18,22 +18,23 @@ qtisas_root = os.path.dirname(os.path.dirname(file))
 
 def parse_clangtidy_output(filename):
     cmd = f'clang-tidy -p {os.path.join(qtisas_root, "build")} {filename}'
-    output = subprocess.check_output(cmd, shell=True, text=True)
+
+    try:
+        output = subprocess.check_output(cmd, shell=True, text=True)
+    except subprocess.CalledProcessError as e:
+        output = e.output
     errors = []
     for line in output.split('\n'):
         if line:
-            if line.startswith('/') and 'warning:' in line:
-                errors.append({})
-                errors[-1]['line'] = line.split(':')[1]
-                errors[-1]['warning'] = line
-                errors[-1]['note'] = ''
-                errors[-1]['fix'] = ''
-            elif line.startswith('/') and 'note:' in line:
-                errors.append({})
-                errors[-1]['line'] = line.split(':')[1]
-                errors[-1]['warning'] = ''
-                errors[-1]['note'] = line
-                errors[-1]['fix'] = ''
+            if line.startswith('/'):
+                errors.append({'line': line.split(':')[1], 'type': '',
+                               'description': line, 'fix': '',})
+            if 'warning:' in line:
+                errors[-1]['type'] = 'warning'
+            elif 'note:' in line:
+                errors[-1]['type'] = 'note'
+            elif 'error:' in line:
+                errors[-1]['type'] = 'error'
             else:
                 errors[-1]['fix'] += line + '\n'
     return errors
@@ -48,16 +49,10 @@ class ClangTidyDB:
 
     def check_filename(self, filename, lines_numbers):
         output = []
-        for entry in self[filename]:
+        for entry in self._errors[filename]:
             if int(entry['line']) in lines_numbers:
                 output.append(entry)
         return output
-
-    def __iter__(self):
-        return iter(self._errors.items())
-
-    def __getitem__(self, filename):
-        return self._errors[filename]
 
 
 if __name__ == '__main__':
@@ -71,23 +66,22 @@ if __name__ == '__main__':
     for filename in latest_changes.keys():
         print(f'\nClang-tidy on {os.path.join(qtisas_root, filename)}')
         warnings.add_filename(os.path.join(qtisas_root, filename))
+    print()
 
     # check for clang-tidy warnings in new lines in filenames
-    print('\n\nClang-tidy has detected several warnings. '
-          'Please fix these warnings for the pipeline to succeed.\n\n')
     status = 0
     for filename in latest_changes.keys():
         entries = warnings.check_filename(os.path.join(qtisas_root, filename),
                                           latest_changes[filename])
         if entries:
-            status = 1
             for entry in entries:
-                if entry["warning"]:
+                if filename in entry['description'] and entry['type'] in ['error', 'warning']:
+                    status = 1
                     print(f'Line {entry["line"]}')
-                    print(f'{entry["warning"]}')
+                    print(f'{entry["description"]}')
                     print(f'Fix:\n{entry["fix"]}')
-                # if entry["note"]:
-                #     print(f'{entry["note"]}')
-                #     print(f'{entry["fix"]}')
 
+    if status:
+        print('\n\nClang-tidy has detected several warnings. '
+              'Please fix these warnings for the pipeline to succeed.\n\n')
     exit(status)
