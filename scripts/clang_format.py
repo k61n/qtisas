88282d035ba3a7ab2795794f76de26f9405796e0
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2024-07-02 18:42:32 +0200

    clang-format: better output
    In case of broken format, the formatter should highlight only the relevant
    code lines and suggest proper formatting for them.

--- scripts/clang_format.py
+++ scripts/clang_format.py
@@ -21,7 +21,7 @@ qtisas_root = os.path.dirname(os.path.dirname(file))
 
 
 def parse_clang_format_output(filenames):
-    """Output format {filename: [{'line_nums': [], 'block': ''}}]"""
+    """Output format {filename: [{'line_nums': [], 'block': ''}]}"""
     cmd_format = lambda filename: f'clang-format -style=Microsoft {filename} ' \
                                   f'> {filename}.formatted'
     cmd_mv = lambda filename: f'mv {filename}.formatted {filename}'
@@ -122,6 +122,27 @@ def get_latest_changes():
     return result
 
 
+class Buffer:
+    def __init__(self, n):
+        self._n = n
+        self._arr = []
+
+    def append(self, value):
+        if len(self._arr) < self._n:
+            self._arr.append(value)
+        else:
+            self._arr = self._arr[1:] + [value]
+
+    def __len__(self):
+        return len(self._arr)
+
+    def __repr__(self):
+        return self._arr.__repr__()
+
+    def __iter__(self):
+        return self._arr.__iter__()
+
+
 if __name__ == '__main__':
     # check the file headers
     status = 0
@@ -140,28 +161,94 @@ if __name__ == '__main__':
     if status:
         exit(status)
 
-    # parse clang-format
+    # parses clang-format
     latest_changes = get_latest_changes()
     formatted = parse_clang_format_output(latest_changes.keys())
-    output_blocks = {}
+    chunks_changed = {}
+    chunks_formatted = {}
+    expressions = {}
+    blocks = {}
 
-    for filename in latest_changes.keys():
-        output_blocks[filename] = []
-        for n in latest_changes[filename]:
-            for block in formatted[filename]:
-                if n in block['line_nums'] \
-                        and block['block'] not in output_blocks[filename]:
-                    output_blocks[filename].append(block['block'])
+    # sees if changed lines appear in clang-format output
+    for fn in latest_changes.keys():
+        # groups subsequently changed lines into chunks
+        chunks_nums = []
+        row_nums = sorted(latest_changes[fn])
+        for i, n in enumerate(row_nums):
+            if i == 0 or row_nums[i - 1] != n - 1:
+                chunks_nums.append([n])
+            else:
+                chunks_nums[-1].append(n)
+        # appends changed chunks to a list
+        # and strips changed chunks from whitespaces for comparison
+        # appends corresponding formatted block for further analysis
+        chunks_changed[fn] = []
+        chunks_formatted[fn] = []
+        expressions[fn] = []
+        blocks[fn] = []
+        for chunk in chunks_nums:
+            chunks_changed[fn].append('')
+            chunks_formatted[fn].append('')
+            expressions[fn].append('')
+            blocks[fn].append('')
+            for n in chunk:
+                l = len(formatted[fn])
+                for i, block in enumerate(formatted[fn]):
+                    if n in block['line_nums'] and block['block'] not in chunks_changed[fn]:
+                        # sometimes git diff fails due to single whitespace change
+                        # just in case add prev and next blocks
+                        # strips color ANSI escape codes
+                        bl = re.sub(r'\x1b\[.*?m', '', formatted[fn][i - 1]['block']) if i else ''
+                        bl += re.sub(r'\x1b\[.*?m', '', block['block'])
+                        bl += re.sub(r'\x1b\[.*?m', '', formatted[fn][i + 1]['block']) if i < l - 1 else ''
+                        # has to match `-  45      :-    if (example_code)`
+                        match = re.compile(rf'-\s*?{n}\s*:-(.*)').search(bl)
+                        chunks_changed[fn][-1] += match.group(0) + '\n'
+                        expressions[fn][-1] += re.sub(r'\s*', '', match.group(1))
+                        blocks[fn][-1] = re.sub(r'-\s*?\d+.*?\n', '', bl)
+            if not blocks[fn][-1]:
+                continue
 
-    status = 0
-    for filename in output_blocks.keys():
-        if output_blocks[filename]:
-            status = 1
-            print('Clang-format found style mismatches in\n'
-                  f'{filename}\n'
-                  'related to the latest push. '
-                  'Please fix them in order to proceed.')
-            for block in output_blocks[filename]:
-                print(block)
+            # parses formatted block to match with expression
+            lines = blocks[fn][-1].split('\n')
+            for i in range(len(lines)):
+                buff = Buffer(i + 1)
+                for line in lines:
+                    match = re.compile(rf'\+\s*\d+:\+(.*)').search(line)
+                    _line = '' if not match else match.group(1)
+                    if not _line:
+                        continue
+                    buff.append(_line)
+                    if len(buff) == i + 1:
+                        if re.sub(r'\s*', '', ''.join(buff)) == expressions[fn][-1]:
+                            chunks_formatted[fn][-1] += '\n'.join(buff) + '\n'
 
-    exit(status)
+    # check if there are format mistmatches
+    status = {}
+    for fn in chunks_changed.keys():
+        status[fn] = 0
+        for chunk in chunks_changed[fn]:
+            if chunk:
+                status[fn] = 1
+                break
+    # generates final output
+    for fn in chunks_changed.keys():
+        if status[fn]:
+            print(f'{fn}\n')
+            for changed, formatted in zip(chunks_changed[fn], chunks_formatted[fn]):
+                if changed:
+                    print('\033[31m' + re.sub(r'\s*?:-.*?\n', '', changed).strip(' ').replace('-', '\n') + '\033[0m')
+                    print('\033[31m' + re.sub(r'-\s*?\d+\s*?:-', '', changed) + '\033[0m')
+                if formatted:
+                    print('\033[32m' + formatted + '\033[0m')
+
+    if 1 in status.values():
+        print('''
+Clang-format found style mismatches related to the latest push.
+Please fix them in order to proceed.
+When you see duplicates, means code fragment repeats.
+Sometimes suggestions might not be present due to `git diff` fail.
+''')
+        exit(1)
+    else:
+        exit(0)
