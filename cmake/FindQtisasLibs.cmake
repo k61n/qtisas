17875abcf3c0ecd3d9f7c373eb214f5e0ba244c4
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2025-10-27 11:14:46 +0100

    cmake: 3party libs are built as cmake targets
    this should simplify development and debugging

--- cmake/FindQtisasLibs.cmake
+++ cmake/FindQtisasLibs.cmake
@@ -1,37 +1,18 @@
 
-# Building 3rdparty qtisas libs
-file(INSTALL FILES
-    ${CMAKE_CURRENT_SOURCE_DIR}/scripts/buildlib.sh ${CMAKE_CURRENT_SOURCE_DIR}/scripts/buildlib.ps1
-    PERMISSIONS OWNER_EXECUTE GROUP_EXECUTE WORLD_EXECUTE DESTINATION ${CMAKE_CURRENT_SOURCE_DIR}/scripts/)
 set(LIBRARIES
     "alglib" "minigzip" "qtexengine" "qwt" "qwtplot3d" "tamuanova")
+set(QTISAS_LIBS "")
 foreach (lib ${LIBRARIES})
-    if (APPLE)
-        execute_process(
-            COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/scripts/buildlib.sh ${OS} ${ARCH} ${CORES} ${lib}
-            ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/${lib}/ ${CMAKE_GENERATOR} ${CMAKE_MAKE_PROGRAM}
-            ${CMAKE_C_COMPILER} ${CMAKE_CXX_COMPILER}
-            ${CMAKE_VERSION} ${GSL_ROOT} ${gl2ps_ROOT} "${CMAKE_PREFIX_PATH}" ${V} ${CMAKE_OSX_SYSROOT})
-    elseif (UNIX)
-        execute_process(
-            COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/scripts/buildlib.sh ${OS} ${ARCH} ${CORES} ${lib}
-            ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/${lib}/ ${CMAKE_GENERATOR} ${CMAKE_MAKE_PROGRAM}
-            ${CMAKE_C_COMPILER} ${CMAKE_CXX_COMPILER}
-            ${CMAKE_VERSION} ${GSL_ROOT} ${gl2ps_ROOT} "${CMAKE_PREFIX_PATH}" ${V})
-    elseif (WIN32)
-        execute_process(
-            COMMAND powershell -ExecutionPolicy Bypass -File ${CMAKE_CURRENT_SOURCE_DIR}/scripts/buildlib.ps1
-            -os ${OS} -arch ${ARCH} -cores ${CORES} -name ${lib} -libdir ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/${lib}/
-            -cc ${CMAKE_C_COMPILER} -cxx ${CMAKE_CXX_COMPILER} -make ${CMAKE_MAKE_PROGRAM} -gen ${CMAKE_GENERATOR}
-            -qt ${CMAKE_PREFIX_PATH} -zlib ${ZLIB_ROOT} -gsl ${GSL_ROOT} -gl2ps ${gl2ps_ROOT}
-            RESULT_VARIABLE OUTPUT)
+    add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/${lib})
+    if (BUILD_SHARED_LIBS)
+        list(APPEND QTISAS_LIBS ${lib}_shared)
+    else (TARGET ${lib}_shared)
+        list(APPEND QTISAS_LIBS ${lib}_static)
     endif ()
+
+    file(GLOB_RECURSE HEADERS "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/${lib}/include/*.h")
+    if(HEADERS)
+        file(COPY ${HEADERS} DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/3rdparty/include/${lib}")
+    endif()
 endforeach ()
-# Importing 3rdparty libs
-foreach (lib ${LIBRARIES})
-    find_library(${lib}_LIBRARY NAMES ${lib} PATHS
-        ${CMAKE_CURRENT_SOURCE_DIR}/libs/${OS}-${ARCH}/${lib}/lib NO_DEFAULT_PATH REQUIRED)
-    message(STATUS "Found ${lib}: ${${lib}_LIBRARY}")
-    set(LINKS ${LINKS} ${${lib}_LIBRARY})
-    include_directories(${CMAKE_CURRENT_SOURCE_DIR}/libs/${OS}-${ARCH}/${lib}/include/)
-endforeach ()
+include_directories(${CMAKE_CURRENT_BINARY_DIR}/3rdparty/include/)
