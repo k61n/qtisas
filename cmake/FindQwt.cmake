d262f28587614f696bec0d6870d1dba24ca21b30
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2026-02-18 09:38:43 +0100

    cmake: min qwt version is 6.3.0 on all platforms

--- cmake/FindQwt.cmake
+++ cmake/FindQwt.cmake
@@ -26,4 +26,18 @@ else()
     include_directories(BEFORE ${qwt_ROOT}/include/)
 endif()
 
-message(STATUS "Found qwt: ${qwt_LIBRARY}")
+# Check Qwt version >= 6.3
+find_file(qwt_GLOBAL_HEADER qwt_global.h
+        PATH_SUFFIXES qwt
+        HINTS ${qwt_ROOT}/include /opt/homebrew/include /usr/include /usr/local/include
+              "${qwt_INCLUDE_WRAPPER}")
+if(qwt_GLOBAL_HEADER)
+    file(STRINGS "${qwt_GLOBAL_HEADER}" qwt_VERSION_LINE REGEX "^#define[ \t]+QWT_VERSION_STR")
+    string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+" qwt_VERSION "${qwt_VERSION_LINE}")
+    message(STATUS "Found qwt: ${qwt_LIBRARY} (found version: ${qwt_VERSION})")
+    if(qwt_VERSION VERSION_LESS "6.3.0")
+        message(FATAL_ERROR "Qwt >= 6.3 is required, but found ${qwt_VERSION}")
+    endif()
+else()
+    message(WARNING "Could not find qwt_global.h to verify Qwt version")
+endif()
